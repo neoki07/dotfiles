42f94477879852cb7121cff486a453154336ffd6
ot07 <ot07neoki@gmail.com>
2023-08-19 22:59:23 +0900

    fix process of swapping caps and ctrl at startup

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -5,7 +5,6 @@ if ! command -v brew &> /dev/null
 then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew already installed."
