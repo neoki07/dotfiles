fbcd9348ca452669b26cad86a8d03ae506fb049b
ot07 <ot07neoki@gmail.com>
2023-08-19 11:22:44 +0900

    setup homebrew

---
+++ Brewfile
@@ -0,0 +1,12 @@
+brew "asdf"
+brew "git"
+brew "neovim"
+brew "starship"
+
+cask "brewlet"
+cask "orbstack"
+cask "tableplus"
+cask "warp"
+
+tap "homebrew/cask-versions"
+cask "visual-studio-code-insiders"
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+set -e
+
+# Set dotfiles directory environment variable
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# Install Homebrew
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed."
+fi
+
+# Install Homebrew packages
+echo "Installing Homebrew packages..."
+brew bundle --file="$DOTFILES_DIR/Brewfile"
