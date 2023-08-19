3bc134d1fe7552e526db193e0f6b22d63334dd8e
ot07 <ot07neoki@gmail.com>
2023-08-19 11:29:47 +0900

    fix install homebrew process

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ if ! command -v brew &> /dev/null
 then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew already installed."
 fi
