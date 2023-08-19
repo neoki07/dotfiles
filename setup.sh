ddcf14063e765396ed06c0fa4aac45abd4c33e38
ot07 <ot07neoki@gmail.com>
2023-08-19 18:57:36 +0900

    remove source ~/.zshrc

--- setup.sh
+++ setup.sh
@@ -8,6 +8,3 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Run scripts
 source "$DOTFILES_DIR/scripts/brew.sh"
 source "$DOTFILES_DIR/scripts/symlink.sh"
-
-# Setup zsh
-source ~/.zshrc
