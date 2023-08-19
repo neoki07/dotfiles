27c6be6b3a56e4828c40d75558060be918663151
ot07 <ot07neoki@gmail.com>
2023-08-19 14:08:38 +0900

    split setup script

---
+++ scripts/brew.sh
@@ -0,0 +1,14 @@
+# Install Homebrew
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Homebrew already installed."
+fi
+
+# Install Homebrew packages
+echo "Installing Homebrew packages..."
+brew bundle --file="$DOTFILES_DIR/scripts/Brewfile"
--- setup.sh
+++ setup.sh
@@ -5,17 +5,5 @@ set -e
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Install Homebrew
-if ! command -v brew &> /dev/null
-then
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-else
-    echo "Homebrew already installed."
-fi
-
-# Install Homebrew packages
-echo "Installing Homebrew packages..."
-brew bundle --file="$DOTFILES_DIR/Brewfile"
+# Run scripts
+source "$DOTFILES_DIR/scripts/brew.sh"
