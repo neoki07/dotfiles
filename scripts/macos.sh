a410b68f9dd34f110ef6a10031673920ee9490c2
ot07 <ot07neoki@gmail.com>
2023-08-19 21:29:40 +0900

    fix cp command bug

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -7,5 +7,5 @@ defaults write -g com.apple.trackpad.scaling 3
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-cp "$DOTFILES_DIR/scripts/SwapCapsCtrl.plist" ~/Library/LaunchAgents
+cp "$DOTFILES_DIR/scripts/SwapCapsCtrl.plist" ~/Library/LaunchAgents/
 launchctl load ~/Library/LaunchAgents/SwapCapsCtrl.plist
