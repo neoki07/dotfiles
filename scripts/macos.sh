3f5e3e1651d71fabc9a78c7c68ee0d506aec80d3
ot07 <ot07neoki@gmail.com>
2023-08-19 22:22:48 -0700

    update macos settings

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -5,6 +5,13 @@ echo "Setting macOS preferences..."
 # Trackpad settings
 defaults write -g com.apple.trackpad.scaling 3
 
+# Mouse settings
+defaults write -g com.apple.mouse.scaling 3
+defaults write -g com.apple.scrollwheel.scaling 5
+
 # Keyboard settings
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Scrollbar settings
+defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
