d82bd6848a13b5e8ff8e97cb37ce9d991efc1c30
ot07 <ot07neoki@gmail.com>
2023-08-21 00:20:11 +0900

    update keyboard settings

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -12,6 +12,7 @@ defaults write -g com.apple.scrollwheel.scaling 5
 # Keyboard settings
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
 
 # Scrollbar settings
 defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
