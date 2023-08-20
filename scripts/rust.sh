aaea370ddbcc7112937552b7b8c63c87ffab2030
ot07 <ot07neoki@gmail.com>
2023-08-20 20:11:31 +0900

    add setting up rust script

---
+++ scripts/rust.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Setting up Rust..."
+if ! command -v rustup &> /dev/null
+then
+    echo "Installing Rustup..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source $HOME/.cargo/env
+else
+    echo "Rustup is already installed"
+fi
+
+if ! command -v rustc &> /dev/null
+then
+    echo "Installing Rust..."
+    rustup install stable
+else
+    echo "Rust is already installed"
+fi
