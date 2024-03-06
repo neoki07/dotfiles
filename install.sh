3d801e7e5b5c494a0bb7bc71616252c13fae71df
neoki <ot07neoki@gmail.com>
2024-03-06 20:20:06 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Install Homebrew
 if ! command -v brew &>/dev/null; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -9,12 +8,26 @@ else
     echo "Homebrew already installed."
 fi
 
-brew install gh
+if ! command -v gh &>/dev/null; then
+    echo "Installing GitHub CLI..."
+    brew install gh
+else
+    echo "GitHub CLI already installed."
+fi
 
-gh auth login -w
+if gh auth status 2>&1 | grep -q "You are not logged into any GitHub hosts."; then
+    gh auth login -w
+else
+    echo "You are already logged in to GitHub."
+fi
 
-gh repo clone neokidev/dotfiles
+if [ -d "$HOME/dotfiles" ]; then
+    echo "Dotfiles already cloned."
+else
+    echo "Cloning dotfiles..."
+    gh repo clone neokidev/dotfiles "$HOME/dotfiles"
+fi
 
-cd dotfiles || exit
+cd "$HOME/dotfiles" || exit
 
-source "setup.sh"
+# source "setup.sh"
