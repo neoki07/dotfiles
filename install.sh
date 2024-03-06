f53d16bc6580fa7c728f7d64c4bc86800fbf8a8d
neoki <ot07neoki@gmail.com>
2024-03-06 19:18:54 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Homebrew
+if ! command -v brew &>/dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Homebrew already installed."
+fi
+
+brew install gh
+
+gh auth login -w
+
+gh repo clone neokidev/dotfiles
+
+cd dotfiles || exit
+
+source "setup.sh"
