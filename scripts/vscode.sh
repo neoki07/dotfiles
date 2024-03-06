236dd0f6b5683d7eaab383038f2cb6cf685d06e8
neoki <ot07neoki@gmail.com>
2024-03-06 23:30:18 +0900

    remove scripts

--- scripts/vscode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Enable key repeat
-# Reference: https://marketplace.visualstudio.com/items?itemName=vscodevim.vim
-defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
