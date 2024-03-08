5f4d64c128e847c08009f08a016033f1711cdb29
neoki <ot07neoki@gmail.com>
2024-03-09 03:39:02 +0900

    disable spotlight shortcuts

--- install.sh
+++ install.sh
@@ -606,6 +606,32 @@ defaults -currentHost write -g "com.apple.keyboard.modifiermapping.$(ioreg -c Ap
 </dict>
 "
 
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "
+<dict>
+  <key>enabled</key><false/>
+  <key>value</key><dict>
+    <key>parameters</key><array>
+      <integer>65535</integer>
+      <integer>49</integer>
+      <integer>1048576</integer>
+    </array>
+    <key>type</key><string>standard</string>
+  </dict>
+</dict>"
+
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 65 "
+<dict>
+  <key>enabled</key><false/>
+  <key>value</key><dict>
+    <key>parameters</key><array>
+      <integer>65535</integer>
+      <integer>49</integer>
+      <integer>1572864</integer>
+    </array>
+    <key>type</key><string>standard</string>
+  </dict>
+</dict>"
+
 # Scrollbar settings
 defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
 
