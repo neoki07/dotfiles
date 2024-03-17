4bd9efff1a6b2c8fd952f4b0f01139f91a023cb2
neoki <ot07neoki@gmail.com>
2024-03-17 23:13:29 +0900

    fix git clone command

--- install.sh
+++ install.sh
@@ -679,7 +679,7 @@ if [ -d "$DOTFILES_DIR" ]; then
   echo "The dotfiles already cloned."
 else
   echo "Cloning the dotfiles..."
-  "ssh -o StrictHostKeyChecking=no" ${GH_COMMAND_PATH} repo clone neokidev/dotfiles "$DOTFILES_DIR"
+  GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" ${GH_COMMAND_PATH} repo clone neokidev/dotfiles "$DOTFILES_DIR"
 fi
 
 # ========================================
