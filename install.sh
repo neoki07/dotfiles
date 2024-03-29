49cdc235aad0f3a590ca6775accb3db073228ee6
neoki <ot07neoki@gmail.com>
2024-03-29 21:49:31 +0900

    wip: run_command_

--- install.sh
+++ install.sh
@@ -88,11 +88,6 @@ run_check_command() {
   return $?
 }
 
-run_command_() {
-  local command=$1
-  eval "$command"
-}
-
 cursor_blink_on() {
   printf "%s[?25h" "$ESC"
 }
@@ -765,7 +760,7 @@ defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 65 "
 
 if ! run_check_command "command -v brew"; then
   echo "Installing Homebrew..."
-  run_command_ "NONINTERACTIVE=1 /bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
