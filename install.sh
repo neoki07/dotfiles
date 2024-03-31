6c622081bb1d95ec1a0a6efa583341f1086d1501
neoki <ot07neoki@gmail.com>
2024-03-31 15:19:09 +0900

    change checkmark

--- install.sh
+++ install.sh
@@ -157,7 +157,7 @@ wait_for_process_to_finish() {
   done
 
   clear_line
-  printf "$STYLE_GREEN%s$STYLE_RESET $done_message" "✓"
+  printf "$STYLE_GREEN%s$STYLE_RESET $done_message\n" "✓"
 
   cursor_blink_on
   stty echo
